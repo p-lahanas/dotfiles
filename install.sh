1373800fd46bcc51465c8088189c00a5e4e44ba0
Peter Lahanas <plahanas18@gmail.com>
2024-06-04 10:03:18 +1000

    Add initial tmux config from https://github.com/craftzdog/dotfiles-public

--- install.sh
+++ install.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-
 # Run this script from the repo root to set configurations
 
 # Define config directory
 export XDG_CONFIG_HOME="$HOME"/.config
 
 # Symbolic links
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/tmux" "$XDG_CONFIG_HOME"/tmux
 
 # Do a user install
 # TODO: Should probs fetch this every install
 mkdir -p $HOME/.local/share/fonts
-cp $PWD/fonts/JetBrainsMono* $HOME/.local/share/fonts
+cp -r $PWD/fonts/JetBrainsMono* $HOME/.local/share/fonts
 
-# apt packages
-#nvim
+# Install required packages
 sudo apt install ripgrep gh
 sudo apt install gcc g++ unzip
 
